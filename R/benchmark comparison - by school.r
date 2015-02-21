051c09c0ba6c813d175a8bbb6984ef7fab3bc307
Sumeet Goil <sumeet@renewschools.org>
2015-02-20 18:04:36 -0600

    STEP W3 Updates

--- R/benchmark comparison - by school.r
+++ R/benchmark comparison - by school.r
@@ -22,15 +22,24 @@ d.l <- select(d.l, subject:scaled_score, percent, on_level, test_name, student_n
 d <- merge(df.step, d.l, by.x='id', by.y='student_number')
 d <- subset(d, !is.na(percent) & !is.na(level) & !is.na(grade))
 
-p <- ggplot(subset(d, test_name == 'B3'), aes(x=factor(level), y=percent))+
+
+make_step_bench_plot_by_school <- function(d, title.string) {
+  ggplot(subset(d, test_name == 'B3'), aes(x=factor(level), y=percent))+
   #geom_boxplot()+
   geom_point(alpha=.5, shape=20, aes(color=achievement_level), position = position_jitter(width = .2))+
   scale_y_continuous(labels=percent)+
   scale_color_manual(values=alPalette)+
   labs(x='STEP',
     y='Percent Correct on DCI Test',
-    title='Benchmark 3 Performance by W3 STEP Level'
+    title= paste0(long_labeller("school", s), ' 2014-15 B3 Performance by W3 STEP Level')
   )+
   theme_bw()+
   facet_grid(subject ~ grade)
-save_plot_as_pdf(p, '2014-15 B3 and STEP W3 Performance')
+}
+
+for(s in schools){
+  ds <- subset(d, school==s)
+  p <- make_step_bench_plot_by_school(ds,s)
+  save_plot_as_pdf(p, paste0('2014-15 B3 and STEP W3 Performance, ',s))
+}
+
